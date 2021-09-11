46dea8eda6c902d482437012c62b66160e637644
theatifwaheed <theatifwaheed@gmail.com>
2021-09-12 00:59:06 +0500

    var

--- variables.dart
+++ variables.dart
@@ -1,9 +1,14 @@
 void main() {
-  var word = "theatifwaheed";
-  var number = 15;
-  var doub = 12.1;
-  bool truefalse = true;
+  var word = "theatifwaheed"; //saves any... according to input...
+  var number = 15; // integer
+  var doub = 12.1; // double
+  var truefalse = true; // bool
 
-  int _num = 5;
-  double _double = 10.8;
+  int _num = 5; // only integer
+  double _double = 10.8; // only double... factornumbers...
+  bool _truefalse = false; // Only True & False
+
+  print("VarWord: $word\nVarNumber: $number\nVarDouble: $doub");
+  print("VarTrueFalse: $truefalse\nIntNum: $_num");
+  print("DoubleNum: $_double\nBoolTrueFalse: $_truefalse");
 }
