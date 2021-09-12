d2aec023ee065e73edb62168e6b82653aeae8139
theatifwaheed <theatifwaheed@gmail.com>
2021-09-12 23:50:38 +0500

    LearnDart.

--- 6_NullableWithCondition.dart
+++ 6_NullableWithCondition.dart
@@ -4,4 +4,13 @@ void main() {
   String _word = word ?? "RIP"; // if there is no null then adds word value
   // if there will be value in word then saves in _word
   print("Word: ${_word}");
+
+  /*
+  // Ignore...
+  String? Word;
+  Word = "Something";
+  String _Word = Word ?? "RIP";
+  print(Word);
+  print(_Word);
+  */
 }
