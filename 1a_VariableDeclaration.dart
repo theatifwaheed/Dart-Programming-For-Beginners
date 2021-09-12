04b19ab05dce68c811a107b400808d30b84442cc
theatifwaheed <theatifwaheed@gmail.com>
2021-09-13 01:06:47 +0500

    VarDec

---
+++ 1a_VariableDeclaration.dart
@@ -0,0 +1,13 @@
+void main() {
+  // This can be used in any DataType.
+  // Correct.
+  int n = 1; // Small Letters
+  int _n = 2; // Underscores in any position
+  int n1 = 3; // Numbers but not in start...
+  int $n = 4; // Dollar Sign in Start but check below to know how to print..
+  int n$ = 5; // Dollar Sign in End.
+  int n_ = 6; // Underscore on any Position
+  int N = 7; // Capital Letters.
+
+  print("$_n $n $n1 ${$n} ${n$} $n_ $N");
+}
