e778a7982ee7ff06ae56d224a3e11cfa33c7f019
theatifwaheed <theatifwaheed@gmail.com>
2021-09-12 22:19:09 +0500

    AllVariables

--- variables.dart
+++ variables.dart
@@ -4,11 +4,12 @@ void main() {
   var doub = 12.1; // double
   var truefalse = true; // bool
 
+  String _word = "theatifwaheed.com";
   int _num = 5; // only integer
   double _double = 10.8; // only double... factornumbers...
   bool _truefalse = false; // Only True & False
 
   print("VarWord: $word\nVarNumber: $number\nVarDouble: $doub");
-  print("VarTrueFalse: $truefalse\nIntNum: $_num");
+  print("VarTrueFalse: $truefalse\nStringWord: $_word\nIntNum: $_num");
   print("DoubleNum: $_double\nBoolTrueFalse: $_truefalse");
 }
