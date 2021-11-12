54ee74329c7c8197fb68a51b3195d7abafba40c2
TheAtifWaheed <theatifwaheed@gmail.com>
2021-11-12 19:58:16 +0500

    ForEach

--- 20a_FOR-Each.dart
+++ 20a_FOR-Each.dart
@@ -1,8 +1,14 @@
 void main() {
   List P = [1, 2, 3, 4, 5, 6];
+  int sum = 0;
+
   P.forEach((element) {
     print(element);
   });
+
+  // P.forEach((element) {
+  //   sum = sum + element;
+  // });
   /* // We Can do this too
   P.forEach((i) {
     print(i);
