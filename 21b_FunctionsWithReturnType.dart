d281a4e1250267606eff452f7998f5b9459d49d6
TheAtifWaheed <theatifwaheed@gmail.com>
2021-11-15 23:06:12 +0500

    ReturnTypes

--- 21b_FunctionsWithReturnType.dart
+++ 21b_FunctionsWithReturnType.dart
@@ -2,6 +2,32 @@ int returnInt() {
   return 21;
 }
 
+bool retT() {
+  return true;
+}
+
+bool retF() {
+  return false;
+}
+
+double retDouble() {
+  return 2.88;
+}
+
+num retNo() {
+  return 8;
+}
+
+String retName() {
+  String name = "theatifwaheed";
+  return name;
+}
+
 void main() {
   print(returnInt());
+  print(retDouble());
+  print(retF());
+  print(retT());
+  print(retNo());
+  print(retName());
 }
