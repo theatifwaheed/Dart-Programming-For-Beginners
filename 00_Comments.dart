c6c845ad345afa269a0e5c541a733281ca0ea346
TheAtifWaheed <theatifwaheed@gmail.com>
2021-11-17 23:30:44 +0500

    New

---
+++ 00_Comments.dart
@@ -0,0 +1,5 @@
+// We can use Comments in Dart by using //
+/*
+Multi Line
+Comments
+*/
---
+++ 01b_Arithmetics.dart
@@ -0,0 +1,19 @@
+/*
+  Some basic Arithmetic Operations. Add, Subtract, Multiply, Divide, & Divisor.
+*/
+
+void main() {
+  int a = 10;
+  int b = 5;
+  int sum = a + b;
+  int sub = a - b;
+  double div = a / b;
+  int divisor = a % b;
+  int mult = a * b;
+
+  print(sum);
+  print(sub);
+  print(div);
+  print(divisor);
+  print(mult);
+}
